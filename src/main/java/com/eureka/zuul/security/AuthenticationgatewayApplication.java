d53d798ab966ad5288fce88658bf93dc2bec1676
Nandan1981 <Nandan1981>
2018-12-04 19:04:30 +0530

    Initial commit

---
+++ src/main/java/com/eureka/zuul/security/AuthenticationgatewayApplication.java
@@ -0,0 +1,12 @@
+package com.eureka.zuul.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationgatewayApplication.class, args);
+    }
+}
